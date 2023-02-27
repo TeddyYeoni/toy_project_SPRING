dec1e975e16655317456e0661ee59d9a688f34ed
leehy <po99027@naver.com>
2023-02-27 14:20:16 +0900

    database 연결 config 코드 작성 및 구현

---
+++ src/main/java/com/toyProject/config/DbConfig.java
@@ -0,0 +1,18 @@
+package com.toyProject.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+	public DataSource dataSource() {
+		DataSource dataSource = new DataSource();
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		dataSource.setUsername("system");
+		dataSource.setPassword("1234");
+		return dataSource;
+	}
+	
+}
