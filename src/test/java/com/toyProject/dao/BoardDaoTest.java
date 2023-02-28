d988df7a2c37d15fb3cf46b3823e8abdf3ffa643
leehy <po99027@naver.com>
2023-02-28 15:10:50 +0900

    boardList 출력 결과 확인

--- src/test/java/com/toyProject/dao/BoardDaoTest.java
+++ src/test/java/com/toyProject/dao/BoardDaoTest.java
@@ -2,10 +2,15 @@ package com.toyProject.dao;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.toyProject.AppTest;
+import com.toyProject.domain.BoardVO;
+
+import lombok.extern.log4j.Log4j;
 
 public class BoardDaoTest extends AppTest{
 
@@ -14,7 +19,8 @@ public class BoardDaoTest extends AppTest{
 	
 	@Test
 	public void boardListTest() {
-		boardDao.boardList();
+		List<BoardVO> boardList = boardDao.boardList();
+		System.out.println(boardList);
 	}
 
 }
