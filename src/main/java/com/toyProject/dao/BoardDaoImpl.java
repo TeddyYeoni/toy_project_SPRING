3a62007c0de47d2fb1ae945cc037f5f75eb6c736
leehy <po99027@naver.com>
2023-02-28 11:35:37 +0900

    BoardDao 메서드 Override

--- src/main/java/com/toyProject/dao/BoardDaoImpl.java
+++ src/main/java/com/toyProject/dao/BoardDaoImpl.java
@@ -1,5 +1,41 @@
 package com.toyProject.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.toyProject.domain.BoardVO;
+
 public class BoardDaoImpl implements BoardDao {
+	
+	SqlSessionTemplate sessionTemplate;
+
+	@Override
+	public List<BoardVO> boardList() {
+		return null;
+	}
+
+	@Override
+	public BoardVO findByBno(Long bno) {
+		return null;
+	}
+
+	@Override
+	public int getNewBno() {
+		return 0;
+	}
+
+	@Override
+	public int addContent() {
+		return 0;
+	}
+
+	@Override
+	public void modifyContent(BoardVO boardVO) {
+	}
+
+	@Override
+	public void deleteContent(Long bno) {
+	}
 
 }
