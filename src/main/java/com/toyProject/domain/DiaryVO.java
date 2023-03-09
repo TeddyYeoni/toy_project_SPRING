0bc18f9bc8b139a125c16b6619c7b0f285389868
leehy <po99027@naver.com>
2023-03-09 14:12:38 +0900

    DairyVO 구성

--- src/main/java/com/toyProject/domain/DiaryVO.java
+++ src/main/java/com/toyProject/domain/DiaryVO.java
@@ -1,5 +1,29 @@
 package com.toyProject.domain;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class DiaryVO {
+
+	private int dno;
+	private String writer;
+	private String content;
+	private String weather;
+	private String emotion;
+	private String praise;
+	private String thanks;
+	private Date writeDate;
 
 }
