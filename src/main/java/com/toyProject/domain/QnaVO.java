9cb1e0615693207f5abf81b02736e19c8ce05d85
leehy <po99027@naver.com>
2023-03-09 14:11:54 +0900

    QnAVO 구성

--- src/main/java/com/toyProject/domain/QnaVO.java
+++ src/main/java/com/toyProject/domain/QnaVO.java
@@ -1,5 +1,27 @@
 package com.toyProject.domain;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class QnaVO {
+
+	private int qno;
+	private String title;
+	private String content;
+	private String writer;
+	private String imageFileName;
+	private Date writeDate;
 
 }
