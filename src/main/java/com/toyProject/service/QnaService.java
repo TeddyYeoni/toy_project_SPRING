6b53aa0fdc9c62ca399f1714e6c8f7e4b47dd7fb
leehy <po99027@naver.com>
2023-03-09 14:49:06 +0900

    Qna 기능 구현을 위한 구조 생성

---
+++ src/main/java/com/toyProject/service/QnaService.java
@@ -0,0 +1,12 @@
+package com.toyProject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.toyProject.dao.QnaDao;
+
+public class QnaService {
+
+	@Autowired
+	private QnaDao qnaDao;
+	
+}
