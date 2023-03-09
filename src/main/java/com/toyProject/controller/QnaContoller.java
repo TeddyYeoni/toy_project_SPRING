bb4677c9660c456afb97fdf421e1fd2a3e6b66c2
leehy <po99027@naver.com>
2023-03-09 14:57:37 +0900

    qnaList 기능 구현

--- src/main/java/com/toyProject/controller/QnaContoller.java
+++ src/main/java/com/toyProject/controller/QnaContoller.java
@@ -1,5 +1,26 @@
 package com.toyProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.toyProject.domain.paging.Criteria;
+import com.toyProject.service.QnaService;
+
+@Controller
+@RequestMapping("/qna")
 public class QnaContoller {
+
+	@Autowired
+	private QnaService qnaService;
+
+	@GetMapping(value = { "", "/", "/qna" })
+	public String list(Model model, @ModelAttribute("cri") Criteria criteria) {
+		model.addAttribute("qna_list", qnaService.qnaList(criteria));
+		return "qna/qnaList";
+	}
 
 }
--- src/main/java/com/toyProject/service/QnaService.java
+++ src/main/java/com/toyProject/service/QnaService.java
@@ -1,12 +1,22 @@
 package com.toyProject.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.toyProject.dao.QnaDao;
+import com.toyProject.domain.QnaVO;
+import com.toyProject.domain.paging.Criteria;
 
+@Service
 public class QnaService {
 
 	@Autowired
 	private QnaDao qnaDao;
-	
+
+	public List<QnaVO> qnaList(Criteria criteria) {
+		return qnaDao.qnaList(criteria);
+	}
+
 }
