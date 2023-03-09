cf4a6343d97578ba084db919a30012cb0307462a
leehy <po99027@naver.com>
2023-03-09 14:13:01 +0900

    AlbumVO 구성

--- src/main/java/com/toyProject/domain/AlbumVO.java
+++ src/main/java/com/toyProject/domain/AlbumVO.java
@@ -1,5 +1,26 @@
 package com.toyProject.domain;
 
-public class AlbumVO {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class AlbumVO {
+	
+	private int ano;
+	private String title;
+	private String content;
+	private String imageFileName;
+	private Date uploadDate;
+	
 }
