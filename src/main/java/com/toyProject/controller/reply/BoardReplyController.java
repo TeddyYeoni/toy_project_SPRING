9704b2dda045bc121d437a7c709a27800baf3984
leehy <po99027@naver.com>
2023-04-04 09:30:21 +0900

    Board 댓글 Controller 구현

--- src/main/java/com/toyProject/controller/reply/BoardReplyController.java
+++ src/main/java/com/toyProject/controller/reply/BoardReplyController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +28,20 @@ public class BoardReplyController {
 	public List<BoardReplyVO> boardReplyList(@RequestParam("bno") Long bno) {
 		List<BoardReplyVO> boardReplyList = boardReplyService.boardReplyList(bno);
 		return boardReplyList;
+	}
+	
+	@PostMapping("/add")
+	@ResponseBody
+	public String insert(@RequestBody BoardReplyVO boardReplyVO) {
+		boardReplyService.addBoardReply(boardReplyVO);
+		return "success";
+	}
+	
+	@PostMapping("/remove")
+	@ResponseBody
+	public String delete(@RequestParam("b_rno") Long b_rno,@RequestParam("bno") Long bno) {
+		boardReplyService.removeBoardReply(b_rno, bno);
+		return "success";
 	}
 
 }
