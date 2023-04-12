5b75bf32b67a2968b6a0f3669a11996b6f4aa4dd
leehy <po99027@naver.com>
2023-04-12 15:21:09 +0900

    CheckList 추가 및 완료 카운트 기능 구현

--- WebContents/WEB-INF/views/toDoList/toDoAddForm.jsp
+++ WebContents/WEB-INF/views/toDoList/toDoAddForm.jsp
@@ -3,22 +3,29 @@
 
 <%@ include file="../layout/header.jsp"%>
 
-<link rel="stylesheet" type="text/css" href="${contextPath}/resources/css/todoStyle.css">
+<link rel="stylesheet" type="text/css"
+	href="${contextPath}/resources/css/todoStyle.css">
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+</script>
 <script src="${contextPath}/resources/js/toDoList/toDoAddForm.js"></script>
 
 <div class="container my-5">
 	<div class="text-center my-5">
 		<h1>Today's To Do List✏</h1>
 	</div>
-	<b>Date</b>&nbsp;&nbsp;&nbsp;<b>${todayDate}</b>
-	<h4>Check List✔</h4>
-	<div class="page-content page-container my-3" id="page-content">
+	<div class="text-center">
+		<b>Date</b>&nbsp;&nbsp;&nbsp;<b>${todayDate}</b>
+	</div>
+	<div class="page-content page-container" id="page-content">
 		<div class="padding">
 			<div class="row container d-flex justify-content-center">
 				<div class="col-md-12">
 					<div class="card px-3">
 						<div class="card-body">
-							<div class="add-items d-flex my-3">
+							<h4>Check List✔</h4>
+							<h4 class="card-title">해야할 일을 추가해보세요!</h4>
+							<div class="add-items d-flex">
 								<input type="text" class="form-control todo-list-input"
 									placeholder="What do you need to do today?">
 								<button
@@ -26,18 +33,7 @@
 							</div>
 							<div class="list-wrapper">
 								<ul class="d-flex flex-column-reverse todo-list">
-									<li>
-										<div class="form-check d-flex justify-content-between">
-											<div class="checkLabael">
-												<label class="form-check-label my-2">체크리스트<i
-													class="input-helper"></i></label>
-											</div>
-											<div class="checkListBtn text-right">
-												<button class='btn btn-outline-primary checkMod'>modify</button>
-												<button class='btn btn-outline-danger checkDel'>delete</button>
-											</div>
-										</div> <i class="remove mdi mdi-close-circle-outline"></i>
-									</li>
+
 								</ul>
 							</div>
 						</div>
--- WebContents/resources/js/toDoList/toDoAddForm.js
+++ WebContents/resources/js/toDoList/toDoAddForm.js
@@ -1,23 +1,45 @@
-$(function checkListCount() {
-  
-    var checkNum = 0;
-    var todoListItem = $('.todo-list');
-    var todoListInput = $('.todo-list-input');
-    
-    $('.todo-list-add-btn').on("click", function(event) {
-      event.preventDefault();
+$(function () {
+    'use strict';
+    $(function () {
+        var todoListItem = $('.todo-list');
+        var todoListInput = $('.todo-list-input');
+        var checkNum = 0; // checkList 목록 개수
+        var completedNum = 0; // 완료한 목록 개수
 
-      var item = $(this).prevAll('.todo-list-input').val();
+        $('.todo-list-add-btn').on("click", function (event) {
+            event.preventDefault();
 
-      if (item) {
-        todoListItem.append("<li><div class='form-check'><label class='form-check-label my-2'>"
-        					+ item + "<i class='input-helper'></i></label></div>"
-        					+ "<i class='remove mdi mdi-close-circle-outline'></i></li>");
-        todoListInput.val("");
-      }
-      
-      checkNum ++;
-      
-   }); // on click end
- }); // checkListCount end
-  
+            var item = $(this).prevAll('.todo-list-input').val();
+
+            if (item) {
+                todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>"
+                    + item + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
+                todoListInput.val("");
+                checkNum++;
+      			console.log('checkList 개수 : '+checkNum);
+            } else {
+                alert('내용을 입력해주세요 :)');
+            }
+        });
+
+        todoListItem.on('change', '.checkbox', function () {
+            if ($(this).attr('checked')) {
+                $(this).removeAttr('checked');
+                completedNum--;
+            } else {
+                $(this).attr('checked', 'checked');
+                completedNum++;
+            }
+        	console.log('완료한 개수 : ' + completedNum);
+            $(this).closest("li").toggleClass('completed');
+
+        });
+
+        todoListItem.on('click', '.remove', function () {
+            $(this).parent().remove();
+            checkNum--;
+            console.log('checkList 개수 : '+checkNum);
+        });
+
+    });
+});
